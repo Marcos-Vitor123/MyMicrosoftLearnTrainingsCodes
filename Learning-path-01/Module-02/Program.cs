4cfb96517e75c6107e5208b7f3ec249606436028
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-11-28 22:14:06 -0300

    Adicionando novos arquivos módulos 01 ao 03

---
+++ Learning-path-01/Module-02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Desafio: Armazenar e recuperar dados usando valores literais e variáveis em C#
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string name = "Bob";
+        int messages = 3;
+        decimal temperature = 34.4m;
+
+        Console.Write("Hello, ");
+        Console.Write(name);
+        Console.Write("! You have ");
+        Console.Write(messages);
+        Console.Write(" messages in your inbox. The temperature is ");
+        Console.Write(temperature);
+        Console.Write(" celsius.");
+    }
+}
