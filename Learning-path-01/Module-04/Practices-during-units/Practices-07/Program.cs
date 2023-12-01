27568e6d9f29a2dcd21e940817f5fa1fde6387b4
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 20:24:03 -0300

    Módulo 04 - pratices 07

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-07/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}"); // Esse é divisível pelo divisor
+        Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+    }
+}
