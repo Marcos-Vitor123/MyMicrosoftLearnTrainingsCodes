d67da65f5f83a827f76145bfcc094b2554ae7365
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 20:16:53 -0300

    Módulo 04 - pratices 06

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-06/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int first = 7;
+        int second = 5;
+        //int quotient = first / second; // Sem conversão de dados = resultado: 1
+        decimal quotient = (decimal)first / (decimal)second; // Conversão de dados = resultado: 1,4
+        Console.WriteLine(quotient);
+    }
+}
