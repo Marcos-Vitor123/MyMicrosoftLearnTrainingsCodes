4d2c6f03fb5b252301320642fb2c64b80a934f0e
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 19:05:56 -0300

    Módulo 04 - pratices 04

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-04/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int sum = 7 + 5;
+        int difference = 7 - 5;
+        int product = 7 * 5;
+        int quotient = 7 / 5;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Diffrence " + difference);
+        Console.WriteLine("Product: " + product);
+        Console.WriteLine("Quotient " + quotient);
+    }
+}
