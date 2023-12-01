9105de71ac5b4de1c9239179e4d7b2bf2812ea00
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 18:56:23 -0300

    Módulo 04 - pratices 02 e 03

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-03/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args) 
+    {
+        string firstName = "Bob";
+        int widgetsSold = 7;
+        Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
+    }
+}
