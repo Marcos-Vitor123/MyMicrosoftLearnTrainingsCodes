7a310104c5041f73880298a9800a0ef1d35ab232
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 19:11:58 -0300

    Módulo 04 - pratices 05

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-05/Program.cs
@@ -0,0 +1,8 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal decimalQuotient = 7.0m / 5;
+        Console.WriteLine($"Decimal quotient: {decimalQuotient}" );
+    }
+}
