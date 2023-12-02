9b25777458be81c7244078164e2e02c47f9c4b14
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 21:11:28 -0300

    Módulo 04 - pratices 08

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-08/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int value1 = 3 + 4 * 5;
+        int value2 = (3 + 4) * 5;
+        Console.WriteLine(value1);
+        Console.WriteLine(value2);
+    }
+}
