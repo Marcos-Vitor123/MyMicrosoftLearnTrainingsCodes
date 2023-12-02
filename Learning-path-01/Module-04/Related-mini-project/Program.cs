501bf0b503510e0699d530447799e9e168f8304f
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 23:49:59 -0300

    Mini projeto - Conversor dólar p/ real - estático

---
+++ Learning-path-01/Module-04/Related-mini-project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        decimal real = 4.88m;
+        decimal dolar = 1.00m;
+        float convert = (float)real * (float)dolar;
+
+        Console.WriteLine($"\tConverter Dólar para Real\t\nValor em Real: {convert} R$");
+    }
+}
