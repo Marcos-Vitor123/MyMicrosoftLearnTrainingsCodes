3c73a091890f219aa9cc3c6ec81e46473f885257
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 23:58:11 -0300

    Mini projeto Convertendo 2 Dólares em real

--- Learning-path-01/Module-04/Related-mini-project/Program.cs
+++ Learning-path-01/Module-04/Related-mini-project/Program.cs
@@ -5,9 +5,10 @@ internal class Program
     private static void Main(string[] args)
     {
         decimal real = 4.88m;
-        decimal dolar = 1.00m;
+        decimal dolar = 2.00m;
+
         float convert = (float)real * (float)dolar;
 
-        Console.WriteLine($"\tConverter Dólar para Real\t\nValor em Real: {convert} R$");
+        Console.WriteLine($"\n\tConvertendo 2 Dólares em Real\t\n\nValor em Real: {convert} R$\n");
     }
 }
