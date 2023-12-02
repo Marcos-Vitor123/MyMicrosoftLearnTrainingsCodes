873bcb44fc73a88bdfe8a9a94ade4925b6751113
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 21:25:20 -0300

    Módulo 04 - pratices 09

--- Learning-path-01/Module-04/Practices-during-units/Practices-08/Program.cs
+++ Learning-path-01/Module-04/Practices-during-units/Practices-08/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         int value1 = 3 + 4 * 5;
-        int value2 = (3 + 4) * 5;
+        int value2 = (3 + 4) * 5; // Priorizando a 1° a soma entre parênteses
         Console.WriteLine(value1);
         Console.WriteLine(value2);
     }
---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-09/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int value = 1; // Recebe o valor 1
+
+        value = value + 1; // Adiciona + 1 passando p/ 2;
+        Console.WriteLine("First increment: " + value);
+
+        value += 1; // Adiciona + 1 passando p/ 3;
+        Console.WriteLine("Second increment: " + value);
+
+        value++; // Adiciona + 1 passando p/ 4;
+        Console.WriteLine("Third increment: " + value);
+
+        value = value -1; // Subtrai - 1 passando p/ 3;
+        Console.WriteLine("First decrement: " + value);
+
+        value -= 1; // Subtrai - 1 passando p/ 2;
+        Console.WriteLine("Second decrement: " + value);
+
+        value--; // Subtrai - 1 passando p/ 1;
+        Console.WriteLine("Third decrement: " + value);
+    }
+}
