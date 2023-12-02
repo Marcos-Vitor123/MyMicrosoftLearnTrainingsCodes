68f505d3788456f951f2e97fc0cb8ed8aedabe8f
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 22:46:06 -0300

    Módulo 04 - pratices 10

---
+++ Learning-path-01/Module-04/Practices-during-units/Practices-10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int value = 1;
+        value++; // Adiciona + 1 passando p/ 2
+
+        Console.WriteLine("First: " + value); // Exibe resultado linha 2
+        Console.WriteLine($"Second: {value++}"); // Incremento posterior: Exibe o valor 2 e adiciona + 1 para exibir na próxima linha
+        Console.WriteLine("Third: " + value); // Recebe o valor Atualizado da linha anterior p/ 3
+        Console.WriteLine("Fourth: " + (++value)); // Incremento antes: Exibe o valor adicionado de + 1 com antecedência
+    }
+}
