4da356ca9c85509c78a4903d9c957aeb7ce4ca53
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-01 22:54:59 -0300

    Desafio Módulo 04

---
+++ Learning-path-01/Module-04/Module-challenge/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int fahrenheit = 94;
+        decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+        Console.WriteLine("The temperature is " + celsius + " Celsius.");
+
+    }
+}
