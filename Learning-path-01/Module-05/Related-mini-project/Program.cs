62e6961e5691079c687eb32a2f050a8c5758821b
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-02 23:45:19 -0300

    Projeto relacionado - Criação inicial

---
+++ Learning-path-01/Module-05/Related-mini-project/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int marcos = 0;
+        int ana = 0;
+        int joao = 0;
+        int pedro = 0;
+
+        Console.WriteLine("\nAlunos\t\tNotas\n");
+        Console.WriteLine($"Marcos:\t\t{marcos}");
+        Console.WriteLine($"Ana:\t\t{ana}");
+        Console.WriteLine($"João\t\t{joao}");
+        Console.WriteLine($"Pedro\t\t{pedro}\n");
+    }
+}
