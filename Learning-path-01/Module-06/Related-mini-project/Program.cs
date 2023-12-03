d2fffdc1b317d27c79ed4b6e8e1c656648a22e61
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-03 09:16:47 -0300

    Módulo 06 - Mini Projeto - Inicial

---
+++ Learning-path-01/Module-06/Related-mini-project/Program.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string estudante1 = "Maria Clara";
+
+        int notaA = 5;
+        int notaB = 4;
+        int notaC = 3;
+        int notaD = 2;
+        int notaE = 1;
+        int notaF = 0;
+
+        int horasCreditoPortugues = 4;
+        int horasCreditoMatematica = 4;
+        int horasCreditoCiencia = 3;
+        int horasCreditoGeografia = 3;
+        int horasCreditoHistoria = 4;
+
+        Console.WriteLine($"\nEstudante: {estudante1}\n");
+        Console.WriteLine("Disciplinas\t\tNotas\tCrédito em Horas\n");
+        Console.WriteLine($"Português\t\t{notaA}\t{horasCreditoPortugues}");
+        Console.WriteLine($"Matemática\t\t{notaB}\t{horasCreditoMatematica}");
+        Console.WriteLine($"Ciência\t\t\t{notaB}\t{horasCreditoCiencia}");
+        Console.WriteLine($"Geografia\t\t{notaD}\t{horasCreditoGeografia}");
+        Console.WriteLine($"História\t\t{notaC}\t{horasCreditoHistoria}\n");
+    }
+}
