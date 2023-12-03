4f0a57433b9281c318a74b0e57a801de1680125c
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-03 06:40:55 -0300

    Módulo 06 - pratices 05

---
+++ Learning-path-01/Module-06/Practices-during-units/Practices-05/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Program 
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string studentName = "Sophia Johnson";
+            string course1Name = "English 101";
+            string course2Name = "Algebra 101";
+            string course3Name = "Biology 101";
+            string course4Name = "Computer Science I";
+            string course5Name = "Psychology 101";
+
+            int course1Credit = 3;
+            int course2Credit = 3;
+            int course3Credit = 4;
+            int course4Credit = 4;
+            int course5Credit = 3;
+
+            int gradeA = 4;
+            int gradeB = 3;
+            int gradeC = 3;
+            int gradeD = 3;
+            int gradeE = 4;
+
+            int course1Grade = gradeA;
+            int course2Grade = gradeB;
+            int course3Grade = gradeC;
+            int course4Grade = gradeD;
+            int course5Grade = gradeE;
+
+            int totalCreditHours = 0;
+
+            totalCreditHours += course1Credit;
+            totalCreditHours += course2Credit;
+            totalCreditHours += course3Credit;
+            totalCreditHours += course4Credit;
+            totalCreditHours += course5Credit;
+
+
+            int totalGradePoints = 0;
+
+            totalGradePoints += course1Credit * course1Grade;
+            totalGradePoints += course2Credit * course2Grade;
+            totalGradePoints += course3Credit * course3Grade;
+            totalGradePoints += course4Credit * course4Grade;
+            totalGradePoints += course5Credit * course5Grade;
+
+            decimal gradePointAverage = (decimal) totalGradePoints / totalCreditHours;
+
+            int leadingDigit = (int) gradePointAverage;
+            int firstDigit = (int) (gradePointAverage * 10) % 10;
+            int secondDigit = (int) (gradePointAverage * 100) % 10;
+            
+            Console.WriteLine($"{course1Name} {course1Grade} {course1Credit}");
+            Console.WriteLine($"{course2Name} {course2Grade} {course1Credit}");
+            Console.WriteLine($"{course3Name} {course3Grade} {course1Credit}");
+            Console.WriteLine($"{course4Name} {course4Grade} {course1Credit}");
+            Console.WriteLine($"{course5Name} {course5Grade} {course1Credit}");
+
+            Console.WriteLine($"Final GPA: {leadingDigit}.{firstDigit}{secondDigit}");
+        }
+    }
+}
