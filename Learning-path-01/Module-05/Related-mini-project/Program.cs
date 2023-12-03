37bfc5b544c75cb5f41b7c3d9153d2928c318fc9
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-02 23:52:00 -0300

    Projeto relacionado - Adicionando soma das notas

--- Learning-path-01/Module-05/Related-mini-project/Program.cs
+++ Learning-path-01/Module-05/Related-mini-project/Program.cs
@@ -2,10 +2,15 @@
 {
     private static void Main(string[] args)
     {
-        int marcos = 0;
-        int ana = 0;
-        int joao = 0;
-        int pedro = 0;
+        int marcos1 = 90, marcos2 = 100, marcos3 = 70, marcos4 = 60;
+        int ana1 = 100, ana2 = 85, ana3 = 80, ana4 = 95;
+        int joao1 = 95, joao2 = 85, joao3 = 100, joao4 = 100;
+        int pedro1 = 100, pedro2 = 95, pedro3 = 80, pedro4 = 70;
+
+        int marcos = marcos1 + marcos2 + marcos3 + marcos4;
+        int ana = ana1 + ana2 + ana3 + ana4;
+        int joao = joao1 + joao2 + joao3 + joao4;
+        int pedro = pedro1 + pedro2 + pedro3 + pedro4;
 
         Console.WriteLine("\nAlunos\t\tNotas\n");
         Console.WriteLine($"Marcos:\t\t{marcos}");
