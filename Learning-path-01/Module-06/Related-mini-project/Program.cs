b9ca1dd3df74fc9cd2533ce34bccdb9f0c0d7d72
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-03 11:17:23 -0300

    Módulo 06 - Mini projeto - GPA

--- Learning-path-01/Module-06/Related-mini-project/Program.cs
+++ Learning-path-01/Module-06/Related-mini-project/Program.cs
@@ -4,12 +4,11 @@
     {
         string estudante1 = "Maria Clara";
 
-        int notaA = 5;
-        int notaB = 4;
-        int notaC = 3;
-        int notaD = 2;
-        int notaE = 1;
-        int notaF = 0;
+        string disciplina1Nome = "Português"; 
+        string disciplina2Nome = "Matemática";
+        string disciplina3Nome = "Ciência";
+        string disciplina4Nome = "Geografia";
+        string disciplina5Nome = "História";
 
         int horasCreditoPortugues = 4;
         int horasCreditoMatematica = 4;
@@ -17,12 +16,45 @@
         int horasCreditoGeografia = 3;
         int horasCreditoHistoria = 4;
 
+        int notaA = 5;
+        int notaB = 4;
+        int notaC = 3;
+        int notaD = 2;
+        int notaE = 1;
+        int notaF = 0;
+
+        int disciplina1Pontos = notaA;
+        int disciplina2Pontos = notaC;
+        int disciplina3Pontos = notaE;
+        int disciplina4Pontos = notaB;
+        int disciplina5Pontos = notaD;
+
+        int totalHorasCreditos = 0;
+
+        totalHorasCreditos += horasCreditoPortugues;
+        totalHorasCreditos += horasCreditoMatematica;
+        totalHorasCreditos += horasCreditoCiencia;
+        totalHorasCreditos += horasCreditoGeografia;
+        totalHorasCreditos += horasCreditoHistoria;
+
+        int totalPontosNotas = 0;
+
+        totalPontosNotas += horasCreditoPortugues * disciplina1Pontos;
+        totalPontosNotas += horasCreditoMatematica * disciplina2Pontos;
+        totalPontosNotas += horasCreditoCiencia * disciplina3Pontos;
+        totalPontosNotas += horasCreditoGeografia * disciplina4Pontos;
+        totalPontosNotas += horasCreditoHistoria * disciplina5Pontos;            
+
+        decimal GPA = (decimal)totalHorasCreditos / totalPontosNotas;
+
         Console.WriteLine($"\nEstudante: {estudante1}\n");
         Console.WriteLine("Disciplinas\t\tNotas\tCrédito em Horas\n");
-        Console.WriteLine($"Português\t\t{notaA}\t{horasCreditoPortugues}");
-        Console.WriteLine($"Matemática\t\t{notaB}\t{horasCreditoMatematica}");
-        Console.WriteLine($"Ciência\t\t\t{notaB}\t{horasCreditoCiencia}");
-        Console.WriteLine($"Geografia\t\t{notaD}\t{horasCreditoGeografia}");
-        Console.WriteLine($"História\t\t{notaC}\t{horasCreditoHistoria}\n");
+        Console.WriteLine($"{disciplina1Nome}\t\t{disciplina1Pontos}\t{horasCreditoPortugues}");
+        Console.WriteLine($"{disciplina2Nome}\t\t{disciplina2Pontos}\t{horasCreditoMatematica}");
+        Console.WriteLine($"{disciplina3Nome}\t\t\t{disciplina3Pontos}\t{horasCreditoCiencia}");
+        Console.WriteLine($"{disciplina4Nome}\t\t{disciplina4Pontos}\t{horasCreditoGeografia}");
+        Console.WriteLine($"{disciplina5Nome}\t\t{disciplina5Pontos}\t{horasCreditoHistoria}\n");
+
+        Console.WriteLine("Nota Final GPA: " + GPA + "\n");
     }
 }
