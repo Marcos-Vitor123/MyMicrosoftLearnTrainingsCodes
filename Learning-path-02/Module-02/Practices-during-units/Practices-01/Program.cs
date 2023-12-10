dc0effe2376c28e87c0d54e31507f1d34db6f0a0
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-10 18:52:24 -0300

    Módulo 02 - Prática 01 + novas anotações README

--- Learning-path-02/Module-02/Practices-during-units/Practices-01/Program.cs
+++ Learning-path-02/Module-02/Practices-during-units/Practices-01/Program.cs
@@ -1,4 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
 
+namespace Practices
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random dice = new Random(); // Random é uma classe 
+            int roll = dice.Next(1, 7); // Next é um método e o 1° parâmetro é o N° inicial e o 2° é o N° final de limite
+            Console.WriteLine($"\n{roll}\n");
 
+            Random dice2 = new Random();
+            int roll2 = dice2.Next(8, 12);
+            Console.WriteLine($"{roll2}\n");
+        }
+    } 
+}
+// Parei em: Unidade 03 - Métodos com estado vs. sem estado
--- Learning-path-02/Module-02/README.md
+++ Learning-path-02/Module-02/README.md
@@ -1,1 +1,10 @@
-# Chamar métodos da Biblioteca de Classes do .NET usando C# 
+# Chamar métodos da Biblioteca de Classes do .NET usando C#
+- Conceitos 
+    - `WriteLine()` é um ***método*** 
+    - `Console` é uma ***classe***
+    - `.` ***Operador*** de acesso de membro
+    - `()` ***Operador*** de chamada do método
+    - ***Argumentos*** são passados dentro dos parênteses do ***operador*** de chamada do método
+    ```cs
+        Console.WriteLine("argumentos aqui");
+    ```
