9bdb8887a89fca2999c009d151ada12d4e27c5c2
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-11 02:22:28 -0300

    Mini projeto módulo 02 PT01

---
+++ Learning-path-02/Module-02/Related-mini-project/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Project
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Random dados = new();
+            int sortear = dados.Next(1, 100);
+            Console.WriteLine(sortear);
+        }
+    } 
+}
