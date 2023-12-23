f2df019cb40ac7e93a8f710fdca8a3bf2a823c65
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-22 22:52:47 -0300

    teste 02

---
+++ Learning-path-02/Module-02/Practices-during-units/Practices-08/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Criando uma instância da classe
+        MinhaClasse minhaClasse = new MinhaClasse();
+
+        // Chamando o método sem estado
+        Console.WriteLine("Método sem estado: " + minhaClasse.MetodoSemEstado());
+
+        // Chamando o método com estado
+        Console.WriteLine("Método com estado: " + minhaClasse.MetodoComEstado());
+        Console.WriteLine("Método com estado: " + minhaClasse.MetodoComEstado());
+        Console.WriteLine("Método com estado: " + minhaClasse.MetodoComEstado());
+    }
+}
+
+class MinhaClasse
+{
+    // Variável de estado
+    private int contador = 0;
+
+    // Método sem estado
+    public int MetodoSemEstado()
+    {
+        return 1;
+    }
+
+    // Método com estado
+    public int MetodoComEstado()
+    {
+        contador++;
+        return contador;
+    }
+}
+
