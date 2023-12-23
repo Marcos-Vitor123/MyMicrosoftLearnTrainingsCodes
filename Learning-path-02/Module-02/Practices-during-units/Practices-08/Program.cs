2c0a10d64a495e7c1e4c42e0df59dbe62b6fb773
Marcos Vitor <dasilvamarcosvitor2@gmail.com>
2023-12-23 00:48:11 -0300

    Módulo 2 - Práticas 8

---
+++ Learning-path-02/Module-02/Practices-during-units/Practices-08/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practices
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            MinhaClasse minhaClasse = new MinhaClasse();
+            
+            Console.WriteLine("Método sem estado: " + minhaClasse.metodoSemEstado());
+
+            Console.WriteLine("Método Com estado: " + minhaClasse.metodoComEstado());
+            Console.WriteLine("Método Com estado: " + minhaClasse.metodoComEstado());
+            Console.WriteLine("Método Com estado: " + minhaClasse.metodoComEstado());
+        }
+    }
+
+    internal class MinhaClasse
+    {
+        private int contador = 0;
+
+        public int metodoSemEstado()
+        {
+            return 1;
+        }
+
+        public int metodoComEstado()
+        {
+            contador++;
+            return contador;
+        }
+    }  
+}
