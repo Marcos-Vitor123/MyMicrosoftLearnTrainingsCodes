592a756472dc9fd69b3dc58f953ca125b2955a06
Marcos Vitor C# | .NET <dasilvamarcosvitor2@gmail.com>
2024-10-09 04:22:19 -0300

    corrigndo bug usando alinhamento

--- Learning-path-03/Module-03/Program.cs
+++ Learning-path-03/Module-03/Program.cs
@@ -16,15 +16,20 @@ public class Program
 
         if((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
         {
-            Console.WriteLine("You rolled doubles! +2 bonus to total!");
-            total += 2;
+            if ((roll1 == roll2) && roll2 == roll3)
+            {
+                Console.WriteLine("You rolled triples! +6 bonus to total!");
+                total += 6;
+            }
+            
+            else
+            {
+                Console.WriteLine("You rolled doubles! +2 bonus to total!");
+                total += 2;
+            }
         }
 
-        if ((roll1 == roll2) && roll2 == roll3)
-        {
-            Console.WriteLine("You rolled triples! +6 bonus to total!");
-            total += 6;
-        }
+        
 
         if (total >= 15)
         {
